2e274fb55f269a75b500e9fd8d4f4fdcfb9270d0
Seth Hayward <seth.hayward@gmail.com>
2012-11-15 17:57:35 -0500

    Initial commit for encouragebot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Encouragebot::Application.config.secret_token = 'e90febbf6ac4cc6b8e9a59f04b872c150b3d8ce95dc7c073453ed1a50454dad97f88521ad7d4f7bd3fb05a83c62a6744a292d25256fab99c11330121b49bc579'
