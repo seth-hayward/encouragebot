2ff5143f62a46b66bb17ae29bf6a5e1139e0a512
Seth Hayward <seth.hayward@gmail.com>
2012-11-16 01:48:25 -0500

    Adding routes, tests, new view for users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
-describe "UserPages" do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get user_pages_index_path
-      response.status.should be(200)
-    end
-  end
+describe "User pages" do
+
+	subject { page }
+
+	shared_examples_for "all user pages" do
+		it { should have_selector("h1", text: heading) }
+		it { should have_selector("title", text: full_title(page_title)) }		
+	end
+
+	describe "signup page" do
+		before { visit signup_path }
+		let(:heading) { 'Sign up' }
+		let(:page_title) { 'sign up' }
+		it_should_behave_like "all user pages"
+	end
+
 end
