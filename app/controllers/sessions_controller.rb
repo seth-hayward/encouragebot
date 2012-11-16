62744386eb9f1ae7ad9d0533e8f828868b8d5c5b
Seth Hayward <seth.hayward@gmail.com>
2012-11-16 16:54:06 -0500

    Logging in, working on remember functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,17 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to user
+		else
+			flash.now[:error] = 'Invalid email/password combination. Please try again.'
+			render 'new'
+		end
 	end
 
 	def destroy
 	end
-	
+
 end
