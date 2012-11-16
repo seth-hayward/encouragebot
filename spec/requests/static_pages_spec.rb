7237a8aaf516a31f1c1cb8e010cc6372a50e4aa5
Seth Hayward <seth.hayward@gmail.com>
2012-11-15 19:48:19 -0500

    Added contact test, view, route and controller action

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -44,4 +44,18 @@ describe "Static pages" do
 
 	end
 
+	describe "Contact page" do
+
+		it "should have the content 'Contact" do
+			visit '/static_pages/contact'
+			page.should have_content('Contact')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', text: "encouragebot - contact")
+		end
+		
+	end
+
 end
