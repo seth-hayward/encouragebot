e1d619643aa8ec5baddaef18e4b4b61409b5cbcb
Seth Hayward <seth.hayward@gmail.com>
2012-11-16 08:59:53 -0500

    Post save tests added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(params[:user])
 		if @user.save
-			flash[:success] = "Welcome to Encourage Bot!"
+			flash[:success] = "Welcome to Encourage Bot!"			
 			redirect_to @user
 		else
 			render 'new'
