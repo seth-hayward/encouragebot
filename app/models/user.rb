8205c52030f5c336de1d9ae5cbe13ea157c06b17
Seth Hayward <seth.hayward@gmail.com>
2012-11-16 03:20:44 -0500

    Lowercase the user email before saving it, again to prevent duplicates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
+  before_save { |user| user.email = email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
