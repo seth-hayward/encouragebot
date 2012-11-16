d73720f1f171d21d602bdb6e48db6b4f551d10e0
Seth Hayward <seth.hayward@gmail.com>
2012-11-15 19:24:17 -0500

    Refactor tests, add tests for title on each page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,14 +9,37 @@ describe "Static pages" do
 			page.should have_content('Encourage Bot')
 		end
 
+		it "should have the right title" do
+			visit '/static_pages/home'
+			page.should have_selector("title", text: "encouragebot - home")
+		end
+
+	end
+
+	describe "Help page" do
+
 		it "should have the content 'Help'" do
 			visit '/static_pages/help'
 			page.should have_content('Help')
 		end
 
+		it "should have the right title" do
+			visit '/static_pages/help'
+			page.should have_selector("title", text: "encouragebot - help")
+		end
+
+	end
+
+	describe "About page" do
+
 		it "should have the content 'About'" do
 			visit '/static_pages/about'
 			page.should have_content('About')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			page.should have_selector("title", text: "encouragebot - about")
 		end
 
 	end
