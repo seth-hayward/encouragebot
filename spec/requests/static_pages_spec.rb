67e194b18910db84995cc0c2c2aa624d4e58e47a
Seth Hayward <seth.hayward@gmail.com>
2012-11-16 00:44:53 -0500

    A slimmer, less verbose test file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,60 +4,35 @@ describe "Static pages" do
 
 	let(:full_title) { "encouragebot" }
 
+	subject { page }
+
 	describe "Home page" do
 		before { visit root_path }
 
-		it "should have the content 'Encourage Bot'" do
-			page.should have_content('Encourage Bot')
-		end
-
-		it "should have the right title" do
-			page.should have_selector("title", text: "#{full_title}")
-		end
-
-		it "should not have a custom page title" do
-			page.should_not have_selector("title", text: "#{full_title} - home")
-		end
-
+		it { should have_content('Encourage Bot') }
+		it { should have_selector("title", text: "#{full_title}") }
+		it { should_not have_selector("title", text: "#{full_title} - home") }
 	end
 
 	describe "Help page" do
 		before { visit help_path }
 
-		it "should have the content 'Help'" do
-			page.should have_content('Help')
-		end
-
-		it "should have the right title" do
-			page.should have_selector("title", text: "#{full_title} - help")
-		end
-
+		it { should have_content('Help') }
+		it { should have_selector("title", text: "#{full_title} - help") }
 	end
 
 	describe "About page" do
 		before { visit about_path }
 
-		it "should have the content 'About'" do
-			page.should have_content('About')
-		end
-
-		it "should have the right title" do
-			page.should have_selector("title", text: "#{full_title} - about")
-		end
-
+		it { should have_content('About') }
+		it { should have_selector("title", text: "#{full_title} - about") }
 	end
 
 	describe "Contact page" do
 		before { visit contact_path }
 
-		it "should have the content 'Contact" do
-			page.should have_content('Contact')
-		end
-
-		it "should have the right title" do
-			page.should have_selector('title', text: "#{full_title} - contact")
-		end
-
+		it { should have_content('Contact') }
+		it { should have_selector('title', text: "#{full_title} - contact") }
 	end
 
 end
