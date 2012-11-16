3354d267f5554796ef53350b58dd950650d320bd
Seth Hayward <seth.hayward@gmail.com>
2012-11-16 07:56:23 -0500

    Handling signup failure now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
-  def new
-  	@user = User.new
-  end
 
   def show
   	@user = User.find(params[:id])
   end
+
+  def new
+  	@user = User.new
+  end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			#successful save
+		else
+			render 'new'
+		end
+	end	  
 end
