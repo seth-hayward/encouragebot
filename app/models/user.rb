050fd0cd0b9fa1a8ddbca06a2743fff6b82addd3
Seth Hayward <seth.hayward@gmail.com>
2012-11-16 04:16:38 -0500

    Very important commit, adds has_secure_password, causes 20 user model tests to go green

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
 
@@ -8,4 +9,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   									uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
