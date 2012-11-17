26f5aab3dc73aed82d6ca06efba939d6ffdfeb1a
Seth Hayward <seth.hayward@gmail.com>
2012-11-17 01:28:02 -0500

    Adding authorization so only signed in users can edit personal profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+	before_filter :signed_in_user, only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
@@ -22,4 +24,22 @@ class UsersController < ApplicationController
 	def edit
 		@user = User.find_by_id(params[:id])
 	end 
+
+	def update
+		@user = User.find_by_id(params[:id])
+		if @user.update_attributes(params[:user])
+			#success
+			flash[:success] = "Profile updated!"
+			sign_in @user
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+		def signed_in_user
+			redirect_to signin_path, notice: "Please sign in." unless signed_in?
+		end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -26,7 +26,7 @@ describe "Authentication" do
 
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
-			before { valid_signin(user) }
+			before { sign_in(user) }
 
 			it { should have_selector('title', text: user.name) }
 			it { should have_link('Profile', href: user_path(user)) }
@@ -41,4 +41,24 @@ describe "Authentication" do
 		end
 	end
 
+	describe "authorization" do
+		describe "for non-signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+
+			describe "in the Users controller" do
+
+					describe "visiting the edit page" do
+						before { visit edit_user_path(user) }
+						it { should have_selector('title', text: 'Sign in') }
+					end
+
+					describe "submitting to the update action" do
+						before { put user_path(user) }
+						specify { response.should redirect_to(signin_path) }
+					end
+
+			end
+
+		end
+	end
 end
