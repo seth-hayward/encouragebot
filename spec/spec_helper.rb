3c755a9a15284dd8bd42a2eada981c056779431a
Seth Hayward <seth.hayward@gmail.com>
2012-11-17 21:54:25 -0500

    Make sure TextHelper is in my test environment so I can use the pluralize function in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ Spork.prefork do
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
+  include ActionView::Helpers::TextHelper
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
