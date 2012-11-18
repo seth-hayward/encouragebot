cdd31bb8dbcf900d3504e5c4c3403e0ecf69e549
Seth Hayward <seth.hayward@gmail.com>
2012-11-17 19:54:58 -0500

    rails generate model Goal title:string user_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121117093822) do
+ActiveRecord::Schema.define(:version => 20121117234456) do
+
+  create_table "goals", :force => true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
