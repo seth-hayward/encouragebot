becfa79061485c6fbff2219de3da59855769a3a0
Seth Hayward <seth.hayward@gmail.com>
2012-11-18 00:01:30 -0500

    Initial goals controller passes tests

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,19 @@
+class GoalsController < ApplicationController
+	before_filter :signed_in_user
+
+	def index
+	end
+
+	def create
+	end
+
+	def destroy
+	end
+
+	def edit
+	end
+
+	def update
+	end	
+
+end
