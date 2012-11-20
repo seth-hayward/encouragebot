3995672792ee90f669d383725fea1fcf303e0ee7
Seth Hayward <seth.hayward@gmail.com>
2012-11-20 10:59:44 -0500

    Change signin route to login to match web standards

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Encouragebot::Application.routes.draw do
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup', to: 'users#new'
-  match '/signin', to: 'sessions#new'
+  match '/login', to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
