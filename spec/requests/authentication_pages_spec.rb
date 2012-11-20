f979a668f564c6a6bf6bc9a1e0d7bb91de1cdc3d
Seth Hayward <seth.hayward@gmail.com>
2012-11-20 13:43:15 -0500

    Add a link, tests for, in main navigation to hidden goals page

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,7 +30,7 @@ describe "Authentication" do
 
 			it { should have_selector('title', text: user.name) }
 			it { should have_link('Sign out', href: signout_path) }
-			#it { should have_link('Users', href: users_path) }
+			it { should have_link('Hidden Goals', href: hidden_path) }
 			it { should_not have_link('Sign in', href: login_path) }
 			it { should have_link('Settings', href: edit_user_path(user)) }
 
